a20b04e564dacd68597744516dbea6a83a46c8e4
GabrielPacco <gpaccohu@unsa.edu.pe>
2024-11-07 09:22:28 -0500

    ignorando dist

--- dragon_attack/src/router/index.js
+++ dragon_attack/src/router/index.js
@@ -4,13 +4,17 @@ import DragonAttackHome from '../components/DragonAttackHome.vue';
 import MathAdventureHome from '../components/MathAdventureHome.vue';
 import AboutDragon from '../components/AboutDragon.vue';
 import VideosSection from '../components/VideosSection.vue';
+import ProcessSection from '../components/ProcessSection.vue';
+import TeamSection from '../components/TeamSection.vue';
 
 const routes = [
-  { path: '/', redirect: '/about-dragon' }, // Redirige temporalmente a AboutDragon para probar
+  { path: '/', redirect: '/dragon-attack' }, // Redirige temporalmente a AboutDragon para probar
   { path: '/dragon-attack', name: 'DragonAttackHome', component: DragonAttackHome },
   { path: '/math-adventure', name: 'MathAdventureHome', component: MathAdventureHome },
-  { path: '/about-dragon', name: 'AboutDragon', component: AboutDragon },
-  { path: '/videos-section', name: 'VideosSection', component: VideosSection },
+  { path: '/about', component: AboutDragon },
+  { path: '/videos', component: VideosSection },
+  { path: '/process', component: ProcessSection },
+  { path: '/team', component: TeamSection },
 ];
 
 const router = createRouter({
