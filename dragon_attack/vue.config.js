44f655f47d047eac1632f4922d9f78ad2b6408ff
GabrielPacco <gpaccohu@unsa.edu.pe>
2024-11-06 22:35:34 -0500

    dist

--- dragon_attack/vue.config.js
+++ dragon_attack/vue.config.js
@@ -2,3 +2,9 @@ const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   transpileDependencies: true
 })
+
+module.exports = {
+  publicPath: process.env.NODE_ENV === 'production'
+    ? '/DragonAttack/' // Reemplaza <nombre-del-repositorio> con el nombre de tu repositorio en GitHub
+    : '/'
+} 
